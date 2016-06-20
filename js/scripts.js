9e7bf95b04bddd9a4e26ce9c945e804901ffca6a
George Olson and Wolfgang Warneke <george@>
2016-06-20 10:04:53 -0700

    add initial empty pet database and pet object constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-/*This file is for your custom js.  All yours*/
+var petDatabase = [];
+
+function Pet(name, type, age, description, medical, profilePic) {
+  this.name = name;
+  this.id = petDatabase.length + 1;
+  this.adopted = false;
+  this.type = type;
+  this.age = age;
+  this.description = description;
+  this.medical = medical;
+  this.profilePic = profilePic;
+}
